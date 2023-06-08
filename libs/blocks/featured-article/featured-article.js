b424ab2f326d1850623f29e324b290772c63c830
Chris Millar <cmillar@adobe.com>
2023-06-08 14:56:08 -0600

    POC

--- libs/blocks/featured-article/featured-article.js
+++ libs/blocks/featured-article/featured-article.js
@@ -25,6 +25,7 @@ export default async function init(el) {
   const categoryTaxonomy = taxonomy.get(category) || 'News';
 
   const pic = doc.body.querySelector('picture');
+  pic.querySelector('img').removeAttribute('loading');
   const featuredImg = createTag('div', { class: 'featured-article-card-image' }, pic);
   const categoryLink = createTag('a', { href: categoryTaxonomy.link }, categoryTaxonomy.name);
   const categoryEl = createTag('div', { class: 'featured-article-card-category' }, categoryLink);
